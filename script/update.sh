628f2e1bac516248daf68afe46cc50e1e778bfa8
Jérôme Rastit <jerome.rastit@libertysurf.fr>
2022-09-14 18:16:05 +0200

    remove backend config

--- script/update.sh
+++ script/update.sh
@@ -8,8 +8,13 @@ function cmd {
 	fi
 }
 cmd "cd alketh/alketh && git pull"
+cmd ". .profile ; cd alketh/alketh/ethers-network ; npm i"
+cmd ". .profile ; cd alketh/alketh/ethers-network ; npm run build"
+cmd ". .profile ; cd alketh/alketh/backend ; npm i"
+cmd ". .profile ; cd alketh/alketh/backend ; npm run build"
 cmd ". .profile ; cd alketh/alketh/contract ; npm i"
 cmd ". .profile ; cd alketh/alketh/contract ; npm run build"
 cmd ". .profile ; cd alketh/alketh/ui ; npm i"
 cmd ". .profile ; cd alketh/alketh/ui ; npm run build"
+cmd ". .profile ; cd alketh/alketh/ui ; pm2 reload alketh-backend"
 cmd ". .profile ; cd alketh/alketh/ui ; pm2 reload alketh"
--- ui/src/config/backendconfig.ts
+++
@@ -1,6 +0,0 @@
-const backendConfig = {
-  url: "http://localhost:8988/api/",
-  urli18n: "http://localhost:8988/assets/i18n/",
-}
-
-export default backendConfig
